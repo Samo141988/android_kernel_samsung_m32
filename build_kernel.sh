b51baac20e378e9dbf2022ce31fea09c7662135b
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-01-22 14:45:28 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -33,7 +33,7 @@ elif [ -z $DEFCONFIG ]; then
 fi
 
 if [[ "$KERNELSU" = "true" ]]; then
-	curl -LSs $KERNELSU_REPO | bash -s main
+	curl -LSs $KERNELSU_REPO | bash -s next
 else
 	pr_info "KernelSU is disabled, export KERNELSU=true to enable it"
 fi
